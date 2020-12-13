74a7c3458016bfcff91e6793cf7ee91f2537fcf3
Benoît Cordier <61388583+cordierben@users.noreply.github.com>
2020-12-13 20:33:21 +0100

    Implémentation Logs
    1/Création de insertLog, permettant d'enregistrer les requêtes dans la BDD, avec l'ip, la date, l'heure, et le contenu. Les paramètres de la fonction sont l'ip du client, et le type de requête (DNS ou DHCP comme dans l'énoncé). Le contenu est juste une phrase avec tous les paramètres.

--- serveur.py
+++ serveur.py
@@ -1,5 +1,6 @@
 import socket
 import sqlite3
+from datetime import date,datetime
 
 
 """
@@ -9,20 +10,23 @@ def ip_creation(mac_adress):
     else:
         print("Access Denied")
  """
-   
+
+
+#Choose the first free IP_Adress, and update it with the mac_adress
 def ip_selection(mac_adress):
     ip_adress_list = SqlDHCP()
     for i in range(len(ip_adress_list)):
         if(ip_adress_list[i][1]==None):
+            InsertLog(ip_adress_list[i],"DHCP")
             InsertMAC(mac_adress,ip_adress_list[i][0])
             return ip_adress_list[i]
     return "No adress available"    
 
 
+#Update Database with the mac adress of the client
 def InsertMAC(mac_adress,ip):
     conn = sqlite3.connect('server.db')
     cur = conn.cursor()
-    print("Connexion réussie à SQLite")
     sql = "UPDATE DHCP SET Mac_Adress=? WHERE IP_Adress=?"
     value = (mac_adress,ip)
     cur.execute(sql, value)
@@ -30,22 +34,48 @@ def InsertMAC(mac_adress,ip):
     print("Enregistrement mis à jour avec succès")
     cur.close()
     conn.close()
-    print("Connexion SQLite est fermée")
+   
     
+#Insert into database the request, the author of the request and datetime
+#IP is the ip adress of the author, request can be "DHCP" or "DNS", depending
+#on the request
+def InsertLog(ip,request):
+    try:
+        conn = sqlite3.connect('server.db')
+        cur = conn.cursor()
+        sql = "INSERT INTO Logs(IP_Adress,Date,Time,Content) VALUES(?,?,?,?)"
+        Date=date.today()
+        Time=datetime.now().time()
+        Content=request+" request by "+str(ip)+" at "+str(Date)+" "+str(Time)
+        value = (str(ip),str(Date),str(Time),Content)
+        cur.execute(sql, value)
+        conn.commit()
+        print("Log enregistré avec succès")
+        cur.close()
+    except sqlite3.Error as error:
+        print("Failed to read data from sqlite table", error)
+    finally:
+        if (conn):
+            conn.close()
+  
+    
+#Retrieve all the logs from the database
 def logs():
+    logList=[]
     try:
         sqliteConnection = sqlite3.connect('server.db')
         cursor = sqliteConnection.cursor()
         cursor.execute("""SELECT * from Logs""")
-        adressList = cursor.fetchall()
+        logList = cursor.fetchall()
         cursor.close()
     except sqlite3.Error as error:
         print("Failed to read data from sqlite table", error)
     finally:
         if (sqliteConnection):
             sqliteConnection.close()
+    return logList
 
-
+#Retrieve all the IP Adress and Mac Adress from the Database
 def SqlDHCP():
     adressList=[]
     try:
@@ -60,10 +90,12 @@ def SqlDHCP():
         if (sqliteConnection):
             sqliteConnection.close()
     return adressList
-    
+
+
 def check(mac_adress):
     None
      #SQL request 
+
 
 def DisplayDatabase(db_file):
     conn = sqlite3.connect(db_file)
@@ -71,6 +103,7 @@ def DisplayDatabase(db_file):
     with conn:
         cur.execute("SELECT * FROM DHCP")
         print(cur.fetchall())
+
 
 if __name__ == '__main__':
 
@@ -93,6 +126,9 @@ if __name__ == '__main__':
     #Confirmation of IP Adress
     confirmation = clientsocket.recv(4096)
     print(confirmation.decode())
+    
+    #Display the logs
+    print(logs)
     
     #DNS : Receive a domain name, and call the socket library to get the IP
     domain = clientsocket.recv(4096)
