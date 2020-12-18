347ee7c3fb67e9a215266b2496ed34d77ff0abbe
Benoît Cordier <61388583+cordierben@users.noreply.github.com>
2020-12-18 14:52:05 +0100

    Bonus DDOS
    Fonction prevent DDOS, appelée à chaque paquet DNS. Récupère les 50 derniers paquets, et vérifie leur heure d'ajout. Si les 50 ont été ajouté en moins d'1 secondes, alors on envoit un email pour DDOS.

--- LogServer/server.py
+++ LogServer/server.py
@@ -2,19 +2,8 @@ from scapy.all import *
 import sqlite3
 import smtplib, ssl
 from datetime import date,datetime
-'''
-Pour utiliser datetime :
 
-datetime.datetime.now()
-datetime.datetime(2020, 12, 16, 14, 46, 19, 443506)
 
-datetime.datetime.now().time()
-datetime.time(14, 46, 32, 566475)
-
-Convert to string: 
-str(datetime.datetime.now().time())
-'14:45:37.410333'
-'''
 def insertLog(log):
     try:
         conn = sqlite3.connect('logserver.db')
@@ -74,6 +63,7 @@ def mail():
     message = """\
     Subject: DDOS ATTEMPT
     
+    
     Server received a lot of request. 
     There is a high probability that a ddos is trying to shutdown the server."""
     
@@ -81,13 +71,47 @@ def mail():
     with smtplib.SMTP_SSL(smtp_server, portEmail, context=context) as server:
         server.login(sender_email, "ProjetNetwork1!")
         server.sendmail(sender_email, receiver_email, message)
+        
+def preventDDOS():
+    timeList=[]
+    try:
+        conn = sqlite3.connect('logserver.db')
+        cur = conn.cursor()
+        cur.execute("SELECT date,time FROM logs WHERE portSrc=53 OR portDST=53 ORDER BY date desc,time desc LIMIT 50")
+        timeList=cur.fetchall()
+    except sqlite3.Error as error:
+        print("Failed to read data from sqlite table", error)
+    finally:
+        if (conn):
+            conn.close()
+    #On vérifie qu'on ait au moins 50 paquets
+    if len(timeList)>49:
+        #On vérifie que les paquets se situent bien sur la même journée
+        if(timeList[0][0]==timeList[49][0]):
+            
+            #On récupère le premier paquet sur 50 et le dernier paquet, qu'on passe format date
+            time = datetime.strptime(timeList[0][1], '%H:%M:%S.%f')
+            time2=datetime.strptime(timeList[49][1], '%H:%M:%S.%f')
+            
+            #On calcule la différence de temps entre les deux 
+            delta=datetime.strptime(str(time-time2), '%H:%M:%S.%f')
+            
+            #On fixe la limite à une seconde
+            now=datetime.now()
+            limit=now.replace(year=1900,month=1,day=1,hour=0, minute=0, second=1, microsecond=0)
+            
+            #Si la différence de temps est inférieure à la limite, on envoit un mail
+            if delta<limit:
+                mail()
             
 
 def mainSniff(p):
 
 
     if(p.dport == 53)or(p.sport == 53):
-
+        
+        preventDDOS()
+            
         log = []
         log.append(p.src)
         log.append(p.dst)
@@ -157,6 +181,7 @@ def mainSniff(p):
 
     if(p.dport == 67)or(p.sport == 68):
 
+        
         #macSrc = p.src
         macSrc = "d0:84:b0:f7:7f:fc"
         macDst = p.dst
